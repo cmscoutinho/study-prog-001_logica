fb0fda7804fbd07bac2e61f8e3ec663d598b83de
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-04-26 23:09:30 -0300

    03_03: added while-loop logic.

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-/* Exercício Aula 01
-
-alert("Boas vindas ao nosso site!");
-
-let nome = "Lua";
-let idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-let mensagemDeErro = "Erro! Preencha todos os campos";
-//alert("Boas-vindas ao jogo do número secreto!");
-alert("Erro! Preencha todos os campos");
-nome = prompt("Qual seu nome?");
-idade = prompt("Qual sua idade?");
-if (idade >= 18) {
-  alert("Pode tirar a habilitação!");
-} 
-
-*/
 alert("Boas-vindas ao jogo do número secreto!");
 let numeroSecreto = 2;
 console.log("Número secreto: " + numeroSecreto);
 
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute;
 console.log("Chute: " + chute);
 
-if (chute == numeroSecreto) {
-  alert(`É isso aí! Você acertou o número secreto ${numeroSecreto}`);
-  console.log("Acertou!");
-} else {
-  alert("Você errou :(");
-  console.log("Errou!");
+while (chute != numeroSecreto) {
+  chute = prompt("Escolha um número entre 1 e 10");
+  if (chute == numeroSecreto) {
+    alert("Isso aí! Você descobriu o número secreto!");
+  } else if (chute > numeroSecreto) {
+    alert(`O número secreto é menor que ${chute}`);
+  } else {
+    alert(`O número secreto é maior que ${chute}`);
+  }
 }
